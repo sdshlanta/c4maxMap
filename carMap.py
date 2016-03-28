e7dd29497aafa5ad57a00a1defb84eea8b17554c
sdshlanta <sdshlnata@gmail.com>
2016-03-28 18:29:19 -0500

    Added multithreading support
    Multithreading support has been added, use the -t flag to define the
    number of threads manually.  uses 4 by default.
    
    Added support for retries, use -r to tell the program to repeat IP
    addresses that time out after a period of time.
    
    the program will now terminate when there are no new hits from both
    Shodan and Censys.  ctrl+c will also work.  On exit the the collected
    data will be output in your chosen format.

--- carMap.py
+++ carMap.py
@@ -1,18 +1,36 @@
+
+from __future__ import print_function
+import argparse
+print = lambda x: sys.stdout.write("%s\n" % x)
+parser = argparse.ArgumentParser(description="Finds TGUs and maps them.")
+parser.add_argument('-K', action='store_true', default=False, help="Specifies the output as KML, the default is HTML")
+parser.add_argument('-H', action='store_true', default=True, help="Specifies the output as HTML, this is the default option.")
+parser.add_argument('-f', type=str, default=None, help='Specifies the filename that should be used. If none is specified the current unix timestamp is used instead')
+parser.add_argument('-d', action='store_true', default=False, help='activates debug mode which increses the verbosity of the printed messages')
+parser.add_argument('-s', type=str, default=None, help='Specifies the use of the Shodan API, please supply your API key as an argument')
+parser.add_argument('-c', type=str, default=None, nargs=2, help='Specifies the use of the Censys API, please supply your API key as an argument. Due to the paged nature of the API this will take some time...')
+parser.add_argument('-t', type=int, default=4, help='Specifies the number of threads that should be used for scanning, defaults to 4.')
+parser.add_argument('-r', action='store_true', default=False, help='Specifies that IP addresses should be retried at a later time if the connection failes.')
+args = parser.parse_args()
+if args.s == None and args.c == None:
+	print('Please specify the use of either the Shodan or Censys API.')
+	exit()
 import telnetlib
 import shodan 
 import json
 from lxml import etree
 from pykml.factory import KML_ElementMaker as KML
 import time
-import argparse
 from bs4 import BeautifulSoup
 import requests
 import threading
 import queue
+import sys
 
+running = True
 fld = KML.Folder()
-
 IPqueue = queue.Queue(maxsize=0)
+outQueue = queue.Queue(maxsize=0)
 
 def degreeConvert(degrees, direction):
 	deg_min, dmin = degrees.split('.')
@@ -25,16 +43,12 @@ def degreeConvert(degrees, direction):
 		return str(-decimal)
 
 def telRequest(ip):
-	try:
-		telnet = telnetlib.Telnet(ip,23,3)
-		print('Connected')
-		telnet.read_until('Basics[', 5)
-		telnet.write("gpspos\n")
-		GPGGASentence = telnet.read_until('Basics[',5)
-		telnet.close()
-		return GPGGASentence
-	except Exception as e:
-		raise e
+	telnet = telnetlib.Telnet(ip,23,5)
+	telnet.read_until('Basics[', 5)
+	telnet.write("gpspos\n")
+	GPGGASentence = telnet.read_until('Basics[',5)
+	telnet.close()
+	return GPGGASentence
 
 class htmlBulider(object):
 	"""docstring for htmlBulider"""
@@ -74,23 +88,27 @@ class censys(object):
 		self.censysApiKey = censysApiKey
 		self.API_URL = "https://www.censys.io/api/v1"
 	def search(self, searchQuery):
+		ips = set()
 		if self.censysApiKey == None:
-				return set()
-		
+				return ips
 		pages = float('inf')
 		page = 1
+		print("Press ctrl+c to proceed with data currently aquired")
 		while page <= pages:
 			try:
-				print('Page: ' + str(page))
+				
 				params = {'query' : searchQuery, 'page' : page}
 				res = requests.post(self.API_URL + "/search/ipv4", json = params, auth = (self.censysApiKey[0], self.censysApiKey[1]))
 				payload = res.json()
-				ips = set()
+
+				
 				for result in payload['results']:
 					ips.add(result['ip'])
 				pages = payload['metadata']['pages']
+				print('Page ' + str(page) + ' of ' + str(pages) + ' (' + str(len(ips)) + ' IPs from Censys so far.)')
 				page+=1
-				time.sleep(1)
+				#because the api is rate limited to 120 requests per 5 min which works out to one request ever 2.5 seconds.
+				time.sleep(2.5)
 			except KeyboardInterrupt as e:
 				break
 			except Exception as e:
@@ -107,20 +125,72 @@ class APIRequests(object):
 		super(APIRequests, self).__init__()
 		self.god = None
 		self.Censys = None
-		try:
-			self.god = shodan.Shodan(shodanApiKey)
-		except shodan.APIError as e:
-			print(e)
+		self.previous = set()
+		if shodanApiKey != None:
+			try:
+				self.god = shodan.Shodan(shodanApiKey)
+			except shodan.APIError as e:
+				print(e)
 		self.Censys = censys(censysApiKey)
 	def search(self, searchQuery):
-		shodanResults = self.god.search(searchQuery)
 		ips = set()
-		for result in shodanResults['matches']:
-			ips.add(result['ip_str'])
-		for ip in ips|self.Censys.search(searchQuery):
+		if self.god != None:
+			shodanResults = self.god.search(searchQuery)
+			for result in shodanResults['matches']:
+				ips.add(result['ip_str'])
+		current = ips|self.Censys.search(searchQuery)
+		difference = current - self.previous
+		if len(difference) == 0:
+			global running
+			running = False
+		print('~' + str(IPqueue.qsize()+len(difference)) + ' IPs to test')
+		for ip in difference:
 			IPqueue.put(ip,True)
+		self.previous = current|self.previous
+
+
+class mapper(object):
+	"""docstring for mapper"""
+	def __init__(self):
+		super(mapper, self).__init__()
+	def run(self):
+		t = threading.Thread(target=self.mapify)
+		t.start()
+		return t
+	def mapify(self):
+		while running:
+			while not IPqueue.empty() and running:
+				try:
+					ip = IPqueue.get(True, 1)
+					#print(ip)
+					GPGGASentence = telRequest(ip)
+					if args.d:
+						print(GPGGASentence)
+					GPGGASentence = GPGGASentence.split("$GPGGA")[1].split(',')
+					lat = degreeConvert(GPGGASentence[2], GPGGASentence[3])
+					lon = degreeConvert(GPGGASentence[4], GPGGASentence[5])
+					print(ip +'\nConnected\nData aquired')
+					outQueue.put((ip, (lon, lat)), True)
+				except IndexError as e:
+					print(ip+'\nConnected\nUnsupported NEMA sentence.')
+					continue
+				except Exception as e:
+					if args.d:
+						print(ip)
+						raise e
+					else:
+						print(ip + '\n' + str(e))
+						if args.r:
+							IPqueue.put(ip,True)
+					continue
+				
+			while IPqueue.empty() and running:
+				pass
+
 
 def main():
+	global running
+
 	if args.H:
 		HTMLBldr = htmlBulider()
 		fileExtention='.html'
@@ -129,58 +199,69 @@ def main():
 		fileExtention='.kml'
 		KMLBldr = kmlBulider()
 
-	APIRequests(args.s,args.c).search('port:23 gps "on console"')
+	APIRequester = APIRequests(args.s,args.c)
+	APIRequester.search('port:23 gps "[1m[35mWelcome on console"')
+	threads = []
 
-	while True:
-		try:
-			while not IPqueue.empty():
-				try:
-					ip = IPqueue.get(True, 3)
-					print("%s" % ip)
-					GPGGASentence = telRequest(ip)
-					if args.d:
-						print(GPGGASentence)
-					GPGGASentence = GPGGASentence.split("$GPGGA")[1].split(',')
-					lat = degreeConvert(GPGGASentence[2], GPGGASentence[3])
-					lon = degreeConvert(GPGGASentence[4], GPGGASentence[5])
-					lonLat = (lon,lat)
+	for x in xrange(args.t):
+		threads.append(mapper().run())
+	for hurp in threads:
+		print(hurp)
+
+	while running:
+		while not IPqueue.empty() and running:
+			try:
+				ip, lonLat = outQueue.get(True,1)
+				if args.K:
+					KMLBldr.addToKML(ip, lonLat)
+				if args.H:
+					HTMLBldr.addToHtml(ip, lonLat)
+			except KeyboardInterrupt:
+				print("Allowing threads to finish up, this should take 30 seconds at most")
+				running = False
+				for thread in threads:
+					thread.join()
+				while not outQueue.empty():
+					ip, lonLat = outQueue.get(True,1)
 					if args.K:
 						KMLBldr.addToKML(ip, lonLat)
 					if args.H:
 						HTMLBldr.addToHtml(ip, lonLat)
-					print("data aquired")
-				except KeyboardInterrupt:
-					raise
-				except Exception as e:
-					print(e)
-					continue
-			try:
-				if args.f == None:
-					f = file(str(time.time()).split('.')[0]+fileExtention, 'w')
-				else:
-					f = file(args.f, 'w')
-				if args.K:
-					f.write(KMLBldr.getKMLString())
-				if args.H:
-					f.write(HTMLBldr.getHTMLString())
-				f.close()
-				print("file generated")
+				continue
+			except queue.Empty:
+				continue
 			except Exception as e:
-				raise e
-		except shodan.APIError as e:
-			print(e)
-			continue
+				if args.d:
+					print(e)
+				continue
+		if running:
+			APIRequester.search('port:23 gps "on console"')
+	try:
+		IPqueue
+		for thread in threads:
+			try:
+				thread.join()
+			except Exception as e:
+				continue
+		while not outQueue.empty():
+			ip, lonLat = outQueue.get(True,1)
+			if args.K:
+				KMLBldr.addToKML(ip, lonLat)
+			if args.H:
+				HTMLBldr.addToHtml(ip, lonLat)
+		if args.f == None:
+			f = file(str(time.time()).split('.')[0]+fileExtention, 'w')
+		else:
+			f = file(args.f, 'w')
+		if args.K:
+			f.write(KMLBldr.getKMLString())
+		if args.H:
+			f.write(HTMLBldr.getHTMLString())
+		f.close()
+		print("file generated")
+	except Exception as e:
+		raise e
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description="Finds TGUs and maps them.")
-	parser.add_argument('-K', action='store_true', default=False, help="Specifies the output as KML, the default is HTML")
-	parser.add_argument('-H', action='store_true', default=True, help="Specifies the output as HTML, this is the default option.")
-	parser.add_argument('-f', type=str, default=None, help='Specifies the filename that should be used. If none is specified the current unix timestamp is used instead')
-	parser.add_argument('-d', action='store_true', default=False, help='activates debug mode which increses the verbosity of the printed messages')
-	parser.add_argument('-s', type=str, default=None, help='Specifies the use of the Shodan API, please supply your API key as an argument')
-	parser.add_argument('-c', type=str, default=None, nargs=2, help='Specifies the use of the Censys API, please supply your API key as an argument. Due to the paged nature of the API this will take some time...')
-	args = parser.parse_args()
-	if args.s == None and args.c == None:
-		print('Please specify the use of either the Shodan or Censys API.')
-		exit()
+	running = True
 	main()
